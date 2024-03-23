c82abd410ab8e0500b50b372295b181ea9b2ff14
Teng Yun <davenkin@126.com>
2024-03-23 18:38:41 +0800

    added consumer

--- src/test/java/davenkin/VanillaKafkaTest.java
+++ src/test/java/davenkin/VanillaKafkaTest.java
@@ -3,9 +3,16 @@ package davenkin;
 import davenkin.user.User;
 import davenkin.utils.KafkaJsonSerializer;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -15,6 +22,7 @@ import java.util.stream.IntStream;
 public class VanillaKafkaTest {
     @Test
     public void produce_message_with_minimum_configuration() {
+        //Producer的最小化配置只需要以下3个配置项
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
@@ -73,4 +81,29 @@ public class VanillaKafkaTest {
         producer.flush();
         producer.close();
     }
+
+    @Test
+    public void consume_message_with_minimum_configuration() throws InterruptedException {
+        this.produce_message_with_minimum_configuration();//准备数据
+
+        //Consumer的最小化配置只需要以下4个配置项
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "consume_message_with_minimum_configuration1");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        consumer.subscribe(List.of("test-topic"));
+        int count = 0;
+        while (count < 10) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                log.info("=====Consumed message with topic[{}],partition[{}],offset[{}]", record.topic(), record.partition(), record.offset());
+            }
+            count++;
+        }
+        consumer.close();
+    }
+
 }
